8ccb2ae5d8d8f19a3b75395f295ea6dbf3727567
Dmitry Ovsianikov <d.ovsianikov@sdventures.com>
2025-10-25 19:14:01 +0200

    Enhance west-build.sh to accept side parameter for building left or right configurations; update corne_choc_pro keymap with home row behaviors and RGB underglow settings in both left and right defconfigs.

--- west-build.sh
+++ west-build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/bash
+
+# Accept side parameter: left or right (default: left)
+SIDE=${1:-left}
+
+if [[ "$SIDE" != "left" && "$SIDE" != "right" ]]; then
+    echo "Error: Side must be 'left' or 'right'"
+    echo "Usage: $0 [left|right]"
+    exit 1
+fi
+
 mkdir -p "/tmp/zmk-config/config"
 cp -R config/* "/tmp/zmk-config/config/"
 west init -l "/tmp/zmk-config/config"
@@ -6,6 +16,15 @@ cd /tmp/zmk-config
 west update --fetch-opt=--filter=tree:0
 west zephyr-export
 build_dir=$(mktemp -d)
-west build -s zmk/app -d $build_dir -b "corne_choc_pro_left" -S "studio-rpc-usb-uart" -- -DZMK_CONFIG=/tmp/zmk-config/config -DSHIELD="nice_view" -DZMK_EXTRA_MODULES='/zmk-config' -DCONFIG_ZMK_STUDIO=y
-cp $build_dir/zephyr/zmk.uf2 /zmk-config/zmk.uf2
+
+if [[ "$SIDE" == "right" ]]; then
+    # Build right side without CONFIG_ZMK_STUDIO
+    west build -s zmk/app -d $build_dir -b corne_choc_pro_right -S studio-rpc-usb-uart -- -DZMK_CONFIG=/tmp/zmk-config/config -DSHIELD=nice_view -DZMK_EXTRA_MODULES=/zmk-config
+else
+    # Build left side with CONFIG_ZMK_STUDIO
+    west build -s zmk/app -d $build_dir -b "corne_choc_pro_left" -S "studio-rpc-usb-uart" -- -DZMK_CONFIG=/tmp/zmk-config/config -DSHIELD="nice_view" -DZMK_EXTRA_MODULES='/zmk-config' -DCONFIG_ZMK_STUDIO=y
+fi
+
+cp $build_dir/zephyr/zmk.uf2 /zmk-config/zmk_${SIDE}.uf2
+echo "Build complete: /zmk-config/zmk_${SIDE}.uf2"
 
